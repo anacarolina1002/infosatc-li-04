62af982103f17fb2cdb368cdc7354e30203fa17c
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-09 18:08:51 -0300

    :monkey: feat: atividade 2

---
+++ atividade2.py
@@ -0,0 +1,12 @@
+#Crie um dicionário com as 5 pessoas que você tem mais afinidade, tendo o nome
+#como chave e a idade como valor.
+
+meuDicionario = {
+ "Elienai"  :  17 , #quando houver mais itens, colocar a vírgula
+ "Lucas"    :  16 ,
+ "Evelyn"   :  16 ,
+ "Luís"     :  17 ,
+ "Helena"   :  15 ,
+}
+for x in meuDicionario:
+    print(x + ":",meuDicionario[x]) 
